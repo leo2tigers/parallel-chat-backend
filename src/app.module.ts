f1e7aa9e1549b4d0d7d323ad18f24569b1305547
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-23 16:37:57 +0700

    :construction: baseline code for each modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GroupModule } from './group/group.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [],
+  imports: [GroupModule, UserModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })

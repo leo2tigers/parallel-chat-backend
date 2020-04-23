6092674ea3256559eccc579b28a6efef65912e8e
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-23 16:59:49 +0700

    :card_file_box: add DB schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,9 @@
+import { Schema } from "mongoose";
+
+export const UserSchema = new Schema({
+    name: String,
+    groupMembership: [{
+        id: Schema.Types.ObjectId,
+        message: [Schema.Types.ObjectId]
+    }],
+})

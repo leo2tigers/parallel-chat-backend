ca9e275037a97a277c854ae2934bbbb8938e488c
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-28 15:39:41 +0700

    :sparkles: add auth systems

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from 'src/schema/user.schema';
-import { UserModule } from 'src/user/user.module';
+import { UserSchema } from '../schema/user.schema';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+          secret: `${process.env.JWT_SECRET}`,
+      }),
+  }),
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt'
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
+
+    async validateUser(username: string, password: string) {
+        const user = await this.userService.getUserByUsername(username);
+        if (!user) {
+            throw new UnauthorizedException(`Invalid username`);
+        }
+        if (await bcrypt.compare(password, user.password)) {
+            return user;
+        }
+        return null;
+    }
+
+    async login(user: any) {
+        const payload = { sub: user._id, username: user.username }
+        return {
+            access_token: this.jwtService.sign(payload),
+            id: user._id,
+            username: user.username,
+        }
+    }
+}

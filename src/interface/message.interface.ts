c57771fb5b3a7df3d5a7f825ad070af47be556e1
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-29 18:54:38 +0700

    :sparkles: add message APIs

--- src/interface/message.interface.ts
+++ src/interface/message.interface.ts
@@ -2,7 +2,6 @@ import { Document, Schema } from 'mongoose';
 
 export interface Message extends Document {
     message: string;
-    sender: Schema.Types.ObjectId;
-    group: Schema.Types.ObjectId;
-    timeSent: Date;
+    sender: string;
+    group: string;
 }
---
+++ src/message/message.dto.ts
@@ -0,0 +1,5 @@
+export class NewMessageDto {
+    message: string;
+    sender: string;
+    group: string;
+}

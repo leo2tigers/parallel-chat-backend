616e2683dbfeb6e2bdd253a92116d0ce07955d21
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-29 16:16:45 +0700

    :sparkles: properly make join/leave group usable.
    - When joining/leaving the change are now reflected in group as well, not just at user side.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from '../schema/user.schema';
+import { GroupModule } from '../group/group.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+        GroupModule
     ],
     controllers: [UserController],
     providers: [UserService],

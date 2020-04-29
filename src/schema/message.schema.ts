5f403d554b69b510d57c71de7f120f1ca25a990d
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-29 16:58:22 +0700

    :art: run formatter

--- src/schema/message.schema.ts
+++ src/schema/message.schema.ts
@@ -1,9 +1,12 @@
 import { Schema } from 'mongoose';
 
-export const MessageSchema = new Schema({
-    message: String,
-    sender: { type: Schema.Types.ObjectId, ref: 'User'},
-    group: { type: Schema.Types.ObjectId, ref: 'Group'},
-}, {
-    timestamps: true
-});
+export const MessageSchema = new Schema(
+    {
+        message: String,
+        sender: { type: Schema.Types.ObjectId, ref: 'User' },
+        group: { type: Schema.Types.ObjectId, ref: 'Group' },
+    },
+    {
+        timestamps: true,
+    },
+);

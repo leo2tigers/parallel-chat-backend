7ecaf750f5ee0fd462edfae1f26dd8672911f975
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-29 18:54:59 +0700

    :art: run formatter

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -7,30 +7,44 @@ import { UserService } from '../user/user.service';
 
 @Injectable()
 export class MessageService {
-    constructor(@InjectModel('Message') private messageModel: Model<Message>, private userService: UserService) {}
+    constructor(
+        @InjectModel('Message') private messageModel: Model<Message>,
+        private userService: UserService,
+    ) {}
 
     async getAllMessage(): Promise<Message[]> {
         return this.messageModel.find();
     }
 
     async getMessageByGroup(groupId: string): Promise<Message[]> {
-        return this.messageModel.find({group: groupId});
+        return this.messageModel.find({ group: groupId });
     }
-    
+
     async getMessageBySender(senderId: string): Promise<Message[]> {
-        return this.messageModel.find({sender: senderId});
+        return this.messageModel.find({ sender: senderId });
     }
 
     private async getLastReadTimeOfAGroup(groupId: string, senderId: string) {
-        return (await this.userService.getUserById(senderId)).groupMembership.find(element => element.group === groupId).group;
+        return (
+            await this.userService.getUserById(senderId)
+        ).groupMembership.find(element => element.group === groupId).group;
     }
 
-    async getUnreadMessageByGroup(groupId: string, senderId: string): Promise<Message[]> {
-        const messages = await this.messageModel.find({group: groupId, sender: senderId, createdTime: {$gte: await this.getLastReadTimeOfAGroup(groupId, senderId)}});
+    async getUnreadMessageByGroup(
+        groupId: string,
+        senderId: string,
+    ): Promise<Message[]> {
+        const messages = await this.messageModel.find({
+            group: groupId,
+            sender: senderId,
+            createdTime: {
+                $gte: await this.getLastReadTimeOfAGroup(groupId, senderId),
+            },
+        });
         await this.userService.updateLastReadOfAGroup(senderId, groupId);
         return messages;
     }
-    
+
     async newMessage(newMessageDto: NewMessageDto) {
         const message = new this.messageModel(newMessageDto);
         return message.save();

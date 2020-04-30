a2b0fe6ca94450293cbb088c5cdeb009a33809d2
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-30 23:38:20 +0700

    :sparkles: Make deleting group more proper
    - Deleting group now delete all message associated and removed link to group from every user that is the member of the deleted group

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -3,12 +3,16 @@ import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GroupSchema } from '../schema/group.schema';
-import { UserModule } from '../user/user.module';
-import { UserSchema } from 'src/schema/user.schema';
+import { UserSchema } from '../schema/user.schema';
+import { MessageSchema } from '../schema/message.schema';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: 'Group', schema: GroupSchema }, {name: 'User', schema: UserSchema}]),
+        MongooseModule.forFeature([
+            { name: 'Group', schema: GroupSchema },
+            { name: 'User', schema: UserSchema },
+            { name: 'Message', schema: MessageSchema}
+        ]),
     ],
     controllers: [GroupController],
     providers: [GroupService],
--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -3,14 +3,15 @@ import { Group } from '../interface/group.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateGroupDto, ChangeGroupNameDto } from './group.dto';
-import { UserService } from '../user/user.service';
 import { User } from '../interface/user.interface';
+import { Message } from '../interface/message.interface';
 
 @Injectable()
 export class GroupService {
     constructor(
         @InjectModel('Group') private groupModel: Model<Group>,
         @InjectModel('User') private userModel: Model<User>,
+        @InjectModel('Message') private messageModel: Model<Message>,
     ) {}
 
     async getAllGroup(): Promise<Group[]> {
@@ -53,6 +54,21 @@ export class GroupService {
     }
 
     async deleteGroupById(id: string) {
+        const userInGroup = await this.userModel.find({
+                'groupMembership.group': id,
+                'groupMembership.lastAccess': { $gte: new Date(-100)}
+            }).exec()
+        userInGroup.forEach(user => {
+            user.groupMembership = user.groupMembership.filter(
+                ({ group, lastAccess }) => {
+                    return group.toString() !== id;
+                },
+            )
+        })
+        userInGroup.forEach(async user => {
+            await user.save();
+        })
+        await this.messageModel.deleteMany({group: id});
         return this.groupModel.findByIdAndDelete(id);
     }
 }

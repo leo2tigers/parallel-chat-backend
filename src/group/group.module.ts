102b24926064f1647a2918e405a7f06db79872af
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-30 12:11:18 +0700

    :bug: fix self-created group could be double-joined

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GroupSchema } from '../schema/group.schema';
+import { UserModule } from '../user/user.module';
+import { UserSchema } from 'src/schema/user.schema';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: 'Group', schema: GroupSchema }]),
+        MongooseModule.forFeature([{ name: 'Group', schema: GroupSchema }, {name: 'User', schema: UserSchema}]),
     ],
     controllers: [GroupController],
     providers: [GroupService],

b7b6efb94a64bd76a16c8debcc76db0bcd0cd257
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-05-01 13:27:25 +0700

    :art: run formatter

--- src/schema/message.schema.ts
+++ src/schema/message.schema.ts
@@ -5,7 +5,7 @@ export const MessageSchema = new Schema(
         message: String,
         sender: { type: Schema.Types.ObjectId, ref: 'User' },
         group: { type: Schema.Types.ObjectId, ref: 'Group' },
-        isJoinOrLeaveMessage: {type: Boolean, default: false},
+        isJoinOrLeaveMessage: { type: Boolean, default: false },
     },
     {
         timestamps: true,

ccec354fcb58782113ec0a57ab03accaa53cc524
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-05-01 15:23:28 +0700

    :ambulance: fix auto-get credentials not working
    - fix auto-get credentials not working due to the change in syntax when upgrading to NestJS 7

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,5 +1,6 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const LoadUser = createParamDecorator((data, req) => {
+export const LoadUser = createParamDecorator((data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
     return req.user;
 });

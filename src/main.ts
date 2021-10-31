e2f46e48fa7fd2c5e6a485392f0393b5a69ac41e
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2021-10-31 20:40:44 +0700

    :sparkles: make cors address customizable

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ require('dotenv').config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+    app.enableCors({
+        origin: process.env.CORS_URI
+    })
     await app.listen(10000);
 }
 bootstrap();
